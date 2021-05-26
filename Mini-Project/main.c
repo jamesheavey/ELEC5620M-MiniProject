2ffb923508a1831496adabb864da04b4f9544471
James Heavey <james@heavey.net>
2021-05-26 23:15:48 +0100

    VGA Testing
    Tested VGA drivers, displaying text and example sprite

--- Mini-Project/main.c
+++ Mini-Project/main.c
@@ -5,6 +5,21 @@
  *      Author: James
  */
 
-int main() {
+#include "DE1SoC_LT24/DE1SoC_LT24.h"
+#include "HPS_Watchdog/HPS_Watchdog.h"
+#include "HPS_usleep/HPS_usleep.h"
+#include "DE1SoC_VGA/DE1SoC_VGA.h"
 
+#include "sprite.h"
+
+int main () {
+	VGA_clearDisplay();
+
+	VGA_drawSprite(dog, 0, 0, 320, 240);
+
+	VGA_drawString("doge", 10, 10);
+
+	while (1) {
+		HPS_ResetWatchdog(); // reset the watchdog.
+	}
 }
---
+++ Mini-Project/sprite.h
@@ -0,0 +1,13 @@
+/*
+ * test_sprite.h
+ *
+ *  Created on: May 26, 2021
+ *      Author: James
+ */
+
+#ifndef SPRITE_H_
+#define SPRITE_H_
+
+extern const unsigned short dog[76800];
+
+#endif /* SPRITE_H_ */
