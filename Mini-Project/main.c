1712e1846d6353394da8ac26e29fafc6e489ec87
James Heavey <james@heavey.net>
2021-05-27 21:00:19 +0100

    Created task handler
    Ship motion now controlled by a task handler, allowing the motion frequency to be controlled by a defined period.

--- Mini-Project/main.c
+++ Mini-Project/main.c
@@ -21,51 +21,87 @@ const int SCREEN_HEIGHT = 240;
 
 volatile unsigned int *LED_ptr = (unsigned int *) 0xFF200000;	// LEDs base address
 
+const unsigned int SCALER = 200 - 1;
+const unsigned int PERIOD = 225000000/(SCALER+1);		// 60 Hz
+const unsigned int NUM_TASKS = 1;
+
+typedef void (*TaskFunction) (int*, int*);
+
+bool left, right, up, down;
+
 void init()
 {
 	PS2_setInterrupt(1);
 
+	Timer_initialise(0xFFFEC600);
+	Timer_setLoad(0xFFFFFFFF);
+	Timer_setControl(SCALER, 0, 1, 1);
+
 	VGA_clearDisplay();
 }
 
-void move_character(int* x)
+void move_ship(int* x, int* y)
+{
+	if (left && *x > 0) {
+		left = 0;
+		*x = *x - 1;
+	} else if (right && *x < SCREEN_WIDTH - PLAYER_WIDTH) {
+		right = 0;
+		*x = *x + 1;
+	} else if (up && *y > 0) {
+		up = 0;
+		*y = *y - 1;
+	} else if (down && *y < SCREEN_HEIGHT - PLAYER_HEIGHT) {
+		down = 0;
+		*y = *y + 1;
+	}
+
+	VGA_drawBGSprite(background, player_ship, *x, *y, PLAYER_WIDTH, PLAYER_HEIGHT);
+}
+
+void PS2_input()
 {
 	unsigned int scancode = PS2_readInput();
 
-	if (scancode == 0xE06B && *x > 0) {
-		*x = *x - 1;
-	} else if (scancode == 0xE074 && *x < SCREEN_WIDTH - PLAYER_WIDTH) {
-		*x = *x + 1;
+	if (scancode == 0xE06B) {
+		left 	= 1;
+	} else if (scancode == 0xE074) {
+		right 	= 1;
+	} else if (scancode == 0xE075) {
+		up 		= 1;
+	} else if (scancode == 0xE072) {
+		down 	= 1;
+	} else {
+		left = 0, right = 0, up = 0, down = 0;
 	}
 }
 
-//void copytoscreenbuffer(const unsigned short* sprite, unsigned int xleft, unsigned int ytop, unsigned int width, unsigned int height)
-//{
-//	int x, y;
-//
-//	for (y = ytop; y < ytop + height; y++) {
-//		for (x = xleft; x < xleft + width; x++) {
-//			screen_buffer[y*(SCREEN_WIDTH) + x] = *sprite++;
-//		}
-//	}
-//}
-
 int main ()
 {
-	int x = 100, y = 0;
-	char byte1 = 0, byte2 = 0;
-	volatile unsigned int byte12;
+	int x = (SCREEN_WIDTH - PLAYER_WIDTH)/2, y =  SCREEN_HEIGHT - PLAYER_HEIGHT - 10;
+
+	int i;
+
+	unsigned int lastIncrTime[NUM_TASKS] = {0};						// all timers start incrementing immediately
+	unsigned int timeValues[NUM_TASKS] = {0};						// all time values initialised to 0
+	const unsigned int incrPeriod[NUM_TASKS] = {PERIOD/100}; 		// set the increment period for all timer units
+	TaskFunction taskFunctions[NUM_TASKS] = {&move_ship};			// define task function struct to call increment functions when required
 
 	init();
 
-	VGA_drawSprite(background, 0, 0, 320, 240);
+	VGA_drawSprite(background, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	VGA_drawString("STAR WARS", 10, 10);
 
 	while (1) {
-		move_character(&x);
+		PS2_input();
 
-		VGA_drawBGSprite(background, player_ship, x, SCREEN_HEIGHT - PLAYER_HEIGHT - 10, PLAYER_WIDTH, PLAYER_HEIGHT);
+		for (i = 0; i < NUM_TASKS; i++) {
+			if ((lastIncrTime[i] - Timer_readValue()) >= incrPeriod[i]) {
+				taskFunctions[i](&x, &y);
+				lastIncrTime[i] -= incrPeriod[i];
+			}
+		}
 
 		HPS_ResetWatchdog(); // reset the watchdog.
 	}
--- Mini-Project/sprites/sprite.h
+++ Mini-Project/sprites/sprite.h
@@ -10,9 +10,9 @@
 
 extern const unsigned short background[76800];
 
-extern const unsigned short player_ship[4440];
+extern const unsigned short player_ship[1800];
 
-const int PLAYER_WIDTH = 60;
-const int PLAYER_HEIGHT = 74;
+const int PLAYER_WIDTH = 50;
+const int PLAYER_HEIGHT = 36;
 
 #endif /* SPRITE_H_ */
